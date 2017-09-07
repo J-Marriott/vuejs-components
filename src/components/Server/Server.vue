2a43d9c34c437f3b4150c8c032279f89ecc6e816
Joe Marriott <joe@j-marriott.com>
2017-09-07 15:33:43 +0100

    can now select server to view status

--- src/components/Server/Server.vue
+++ src/components/Server/Server.vue
@@ -1,12 +1,21 @@
 <template>
-  <li class="list-group-item">
-    Server #{{ id }}
+  <li class="list-group-item"
+    style="cursor: pointer"
+    @click="serverSelected">
+    Server #{{ server.id }}
   </li>
 </template>
 
 <script>
+
+  import { serverBus } from '../../main';
   export default {
-    props: ['id']
+    props: ['server'],
+    methods: {
+      serverSelected() {
+        serverBus.$emit('serverSelected', this.server);
+      }
+    }
   }
 </script>
 
